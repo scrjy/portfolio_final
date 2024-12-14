ad21bb5882dd472de5ce24b1c1d20f92f51aef64
scrjy <sjyplus123@naver.com>
2024-12-14 11:32:24 +0900

    2nd

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,12 +6,36 @@ const Contact = () => {
       <br />
       <p>휴대전화 : 010-0000-0000</p>
       <br />
-      <p>이메일 : google@gmail.com</p>
+      <p>이메일 : email0000@email.com</p>
       <br />
-      <p>카카오톡 ID : kakaotalk</p>
+      <p>카카오톡 ID : abcd0000</p>
       <br />
-      <p>인스타그램 ID : instagram</p>
+      <p>인스타그램 : @instagram0000</p>
       <br />
+      <ul className="mt-4">
+        <li className="mb-4">
+          <p className="mt-2">
+            <a
+              href="https://github.com/scrjy?tab=repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              팀 프로젝트
+            </a>
+          </p>
+        </li>
+        <li className="mb-4">
+          <p className="mt-2">
+            <a
+              href="https://portfolio-beryl-six-36.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              중간고사 포트폴리오
+            </a>
+          </p>
+        </li>
+      </ul>
     </div>
   )
 }
